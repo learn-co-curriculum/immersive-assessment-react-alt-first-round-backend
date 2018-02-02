abebef0caf7d00525f84dffc3752e6b40da58204
maxwell <github email address>
2018-02-02 10:37:13 -0500

    updated landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def index
-    render html: "API for Bayside High Grade Tracker"
+    render html: "<div>API for Bayside Student Lister</div><img width='500px' src='https://www.nbcstore.com/media/catalog/product/cache/1/image/1000x/040ec09b1e35df139433887a97daa66f/s/a/savedbythebell_bayside_tigers_rollover_1__7.png' alt='bayside' />".html_safe
   end
-  
+
 end
