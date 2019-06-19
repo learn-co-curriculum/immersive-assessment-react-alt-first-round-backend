b29887ea66454392840afa498ff3f45f8318843c
Wangtron <evansjwang@gmail.com>
2019-06-19 17:32:33 -0400

    removed byebug

--- app/models/generator.rb
+++ app/models/generator.rb
@@ -10,40 +10,40 @@ class Generator
 
 			num_students.times do
 				new_user = User.create()
-				byebug
+
         newly_created_users << new_user
 
-        course_list = [
-          {
-            name: "Knitting Hats for Cats",
-            instructor: "Tim Campbell",
-            semester: "Winter 2017"
-          }, {
-            name: "Swift for Beginners",
-            instructor: "Joe Burgess",
-            semester: "Summer 2017"
-          }, {
-            name: "Rock Climbing",
-            instructor: "Avi Flombaum",
-            semester: "Spring 2017"
-          }, {
-            name: "History of Ireland",
-            instructor: "Johann Kerr",
-            semester: "Fall 2017"
-          }, {
-            name: "Home Ec",
-            instructor: "Jeff Katz",
-            semester: "Winter 2017"
-          }, {
-            name: "Music Theory",
-            instructor: "Niky Morgan",
-            semester: "Fall 2017"
-          }, {
-            name: "Wilderness Survival",
-            instructor: "Alex Griffith",
-            semester: "Spring 2017"
-          }
-        ]
+				course_list = [
+				  {
+				    name: "Knitting Hats for Cats",
+				    instructor: "Tim Campbell",
+				    semester: "Winter 2017"
+				  }, {
+				    name: "Having a good time",
+				    instructor: "Charles Entertainment Cheese",
+				    semester: "Summer 2017"
+				  }, {
+				    name: "Rock Climbing",
+				    instructor: "Avi Flombaum",
+				    semester: "Spring 2017"
+				  }, {
+				    name: "History of Ireland",
+				    instructor: "Johann Kerr",
+				    semester: "Fall 2017"
+				  }, {
+				    name: "Getting Gainz",
+				    instructor: "Evans Wang",
+				    semester: "Winter 2017"
+				  }, {
+				    name: "Music Theor",
+				    instructor: "Niky Morgan",
+				    semester: "Fall 2017"
+				  }, {
+				    name: "Wilderness Survival",
+				    instructor: "Alex Griffith",
+				    semester: "Spring 2017"
+				  }
+				]
 
 
 				courses = course_list.sample(4).map{|course|
